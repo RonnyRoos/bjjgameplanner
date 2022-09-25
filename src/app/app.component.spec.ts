588a78c3fe3b23afe61f84bca5f6020e08fb067b
Ronny Roos <ronny@ronnyroos.com>
2022-09-25 17:49:48 +0200

    Initial setup of angular/firebase and simple todo app implementation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'bjjgameplanner'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bjjgameplanner');
   });
 
   it('should render title', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {TaskComponent} from './task/task.component';
+import {MatCardModule} from "@angular/material/card";
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatDialogModule} from "@angular/material/dialog";
+import {TaskDialogComponent} from './task-dialog/task-dialog.component';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService} from '@angular/fire/analytics';
+import {provideFirestore, getFirestore} from '@angular/fire/firestore';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService, UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
